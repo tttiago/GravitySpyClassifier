c57ea6bbabae4d8cc5d1cb2afe04c93730abc104
tiago <tiagosf99@gmail.com>
2022-08-16 16:25:34 +0100

    Correct bash file to run best configs.

--- run_best_configs.sh
+++ run_best_configs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..7}
 do
-	python wandb_sweep.py -f "configs/config_06_optimized_scratch_best$i"  
+	python wandb_sweep.py -f "configs/config_06_optimized_scratch_best${i}.json"  
 done 
