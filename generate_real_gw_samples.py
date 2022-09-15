d91a3c70b7fd73b10da40a052436087b25a00ce6
tiago <tiagosf99@gmail.com>
2022-09-15 16:33:37 +0100

    Fix bug: only the H1 detector was used before.

--- generate_real_gw_samples.py
+++ generate_real_gw_samples.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 GRAY_SCALE = True
 DATASET_PATH = './datasets/Real_GWs_BW'
+VMAX = 25.5
 
 O1_events = datasets.find_datasets(
     type="events", catalog="GWTC-1-confident", segment=run_segment("O1")
@@ -24,12 +25,12 @@ for event in tqdm(events):
     gps = event_gps(event)
     segment = (int(gps) - seg_int / 2, int(gps) + seg_int / 2)
     for detector in detectors:
-        data = TimeSeries.fetch_open_data("H1", *segment, sample_rate=16384, cache=True)
+        data = TimeSeries.fetch_open_data(detector, *segment, sample_rate=16384, cache=True)
         for time_window in time_windows:
             hq = data.q_transform(
                 frange=(10, 2048), outseg=(gps - time_window / 2, gps + time_window / 2)
             )
-            plot = hq.plot(figsize=(17, 14), dpi=20, vmin=0, vmax=25.5)
+            plot = hq.plot(figsize=(17, 14), dpi=20, vmin=0, vmax=VMAX)
             plt.yscale("log", base=2)
             ax = plt.gca()
             ax.axis("off")
