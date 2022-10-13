c1674ec5de124d33bbd1f1c695d517c111125233
tiago <tiagosf99@gmail.com>
2022-10-13 14:19:39 +0100

    Add exception to handle cases of unavailable data.

--- generate_real_gw_samples.py
+++ generate_real_gw_samples.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 import sys
 
 GRAY_SCALE = True
-DATASET_PATH = './datasets/Real_GWs_v2'
+DATASET_PATH = './datasets/Real_GWs_O3_v1'
 VMAX = 25.5
 
 O1_events = datasets.find_datasets(
@@ -18,7 +18,14 @@ O1_events = datasets.find_datasets(
 O2_events = datasets.find_datasets(
     type="events", catalog="GWTC-1-confident", segment=run_segment("O2_4KHZ_R1")
 )
-events = O1_events + O2_events
+O3a_events = datasets.find_datasets(
+    type="events", catalog="GWTC-2.1-confident", segment=run_segment("O3a_4KHZ_R1")
+)
+O3b_events = datasets.find_datasets(
+    type="events", catalog="GWTC-3-confident", segment=run_segment("O3b_4KHZ_R1")
+)
+#events = O1_events + O2_events
+events = O3a_events + O3b_events
 
 sample_rate = 16384
 seg_int = 32
@@ -39,12 +46,16 @@ if __name__ == '__main__':
         gps = event_gps(event)
         segment = (np.ceil(gps) - seg_int/2, np.ceil(gps) + seg_int/2)
         for detector in detectors:
-            data = TimeSeries.fetch_open_data(detector, *segment, sample_rate=sample_rate, cache=True, verbose=False)
-            hq = data.q_transform(
-                    frange=(10, 2048), 
-                    outseg=(gps-max(time_windows)/2-window_pad, gps+max(time_windows)/2+window_pad), 
-                    tres=tres
-                )
+            try:
+                data = TimeSeries.fetch_open_data(detector, *segment, sample_rate=sample_rate, cache=True, verbose=False)
+                hq = data.q_transform(
+                        frange=(10, 2048), 
+                        outseg=(gps-max(time_windows)/2-window_pad, gps+max(time_windows)/2+window_pad), 
+                        tres=tres
+                    )
+            except ValueError:
+                print(f"There was a problem with the {event.split('-')[0]}_{detector} signal. Skipping it...")
+                continue
             max_point = int(2*len(hq)/5) + np.argmax(np.max(hq[int(2*len(hq)/5):int(3*len(hq)/5),:], axis=1))
             shift = -max(time_windows)/2-window_pad + max_point * tres
             for time_window in time_windows:
